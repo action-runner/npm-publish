eb008ee7ecec741519fafdc9d3b8862c19a16dd0
sirily11 <sirily1997@gmail.com>
2022-04-19 03:06:46 +0800

    fix: import

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,29 @@
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -15,21 +38,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const core_1 = __importDefault(__nccwpck_require__(2186));
+const core = __importStar(__nccwpck_require__(2186));
 const npm_publisher_1 = __nccwpck_require__(4902);
 (() => __awaiter(void 0, void 0, void 0, function* () {
     // get input from action
-    const registries = core_1.default.getMultilineInput("registries", { required: true });
-    const packageFiles = core_1.default.getMultilineInput("packageFiles", {
+    const registries = core.getMultilineInput("registries", { required: true });
+    const packageFiles = core.getMultilineInput("packageFiles", {
         required: true,
     });
-    const tokens = core_1.default.getMultilineInput("tokens", { required: true });
+    const tokens = core.getMultilineInput("tokens", { required: true });
     if (registries.length !== tokens.length) {
-        core_1.default.setFailed("registries and tokens must have the same length");
+        core.setFailed("registries and tokens must have the same length");
     }
     const publisher = new npm_publisher_1.NpmPublisher({
         registries: registries.map((registry, index) => ({
